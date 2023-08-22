19a04eadb101ad6b08d80c9af41fecb75e546e03
MD:sahariar Ahammed Emon <sahariargd28@gmail.com>
2023-08-22 23:56:41 +0600

    slide add

---
+++ src/Pages/Home/Banner/Banner2/Banner2.jsx
@@ -0,0 +1,30 @@
+
+
+const Banner2 = () => {
+  return (
+    <div className=" mb-10">
+      <div
+        className="hero h-[504px]"
+        style={{
+          backgroundImage:
+            "url(https://i.ibb.co/84hCWNQ/chef-service.jpg)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className=" p-10 justify-center items-center text-center text-black bg-white ">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner2;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,8 +1,14 @@
 import Banner from "../Banner/Banner";
+import Banner2 from "../Banner/Banner2/Banner2";
+import Category from "../category/category";
 
 const Home = () => {
   return (
+    <div>
     <Banner></Banner>
+    <Category></Category>
+    <Banner2></Banner2>
+    </div>
   );
 };
 
